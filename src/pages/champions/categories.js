d64aaa35ee290c81803ad4e3cc60374c11bec208
David Madrid <davidmadridpathway@gmail.com>
2022-04-30 19:39:47 -0500

    Champions page fixed and styled

--- src/pages/champions/categories.js
+++ src/pages/champions/categories.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Categories = ({ categories, filterItems }) => {
   return (
-    <div className="flex flex-wrap justify-center space-x-5 px-5">
+    <div className="h-32 flex flex-wrap justify-center space-x-5 px-5">
       {categories.map((category, index) => {
         return (
-          <button className="text-slate-500 hover:text-black text-base p-3" type="button" key={index} onClick={() => filterItems(category)}>
+          <button className="w-20 py-1 hover:text-xl duration-200 text-slate-500 hover:text-black text-base" type="button" key={index} onClick={() => filterItems(category)}>
             {category}
           </button>
         )
--- src/pages/champions/item.js
+++ src/pages/champions/item.js
@@ -6,9 +6,11 @@ const Item = ({ data }) => {
       {data.map((champion) => {
         const { id, title, img } = champion
         return (
-          <div key={id} className='p-3'>
-            <img src={img} alt={title} />
-            <h3 className='bg-gradient-to-r from-cyan-900 to-violet-900 p-3 text-white text-xl hover:bg-indigo-900'>{title}</h3>
+          <div key={id} className='m-3 cursor-pointer'>
+            <div className='overflow-hidden'>
+              <img className='hover:scale-110 duration-200' src={img} alt={title} />
+            </div>
+            <h3 className='font-bold tracking-wider bg-neutral-900 p-3 text-white text-xl'>{title}</h3>
           </div>
         );
       })}
