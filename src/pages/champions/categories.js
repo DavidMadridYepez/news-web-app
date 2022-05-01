d3a8f515aef7fe3b660bbc60ef3a7c511a5b3dd4
David Madrid <davidmadridpathway@gmail.com>
2022-04-30 19:42:22 -0500

    Fixed champions page

--- src/pages/champions/categories.js
+++ src/pages/champions/categories.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Categories = ({ categories, filterItems }) => {
   return (
-    <div className="h-32 flex flex-wrap justify-center space-x-5 px-5">
+    <div className="h-32 md:h-10 flex flex-wrap justify-center space-x-5 px-5">
       {categories.map((category, index) => {
         return (
           <button className="w-20 py-1 hover:text-xl duration-200 text-slate-500 hover:text-black text-base" type="button" key={index} onClick={() => filterItems(category)}>
